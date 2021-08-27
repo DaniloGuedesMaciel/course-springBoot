52fedd5ba72a7665dcd6a29f7da493a293f8482c
Danilo Guedes Maciel <dan.gmaciel@gmail.com>
2021-08-26 23:52:45 -0300

    Service layer, component registration

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -23,7 +23,7 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(Integer id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -32,11 +32,11 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- src/main/java/com/example/course/resource/UserResource.java
+++ src/main/java/com/example/course/resource/UserResource.java
@@ -1,19 +1,33 @@
 package com.example.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.course.entities.User;
+import com.example.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Danilo", "danilo@gmail.com", "123456789", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
